a8c926098a642607338eecf28423122fb6a4e921
joabe da conceição <joabeercg2012@gmail.com>
2025-08-19 20:32:18 +0000

    batalhaNaval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,14 +3,20 @@
 
 int main() {
     char linha[10]= {'A' ,'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    
+    int i = 0;
+    int j = 0;
     // Criando o tabuleiro (Matriz 10x10)
     
      int tabuleiro[10][10];
      printf("Tabuleiro batalha naval\n");
      for (int i = 0; i < 10; i++) {
+
+        for (int j = 0; j < 10; j++) {
         
-        printf("%c", linha[i]);
+        printf("%c", linha[i][j]);
+        
+        }
+        
      }
     return 0;
 }
